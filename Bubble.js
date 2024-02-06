9f7e937ef9e47384e7caa525026c5d5f95431489
sphilips04 <sphilips@email.sc.edu>
2024-02-06 17:10:42 -0500

    changed bubble algorithm

--- Bubble.js
+++ Bubble.js
@@ -1,38 +1,25 @@
 class Bubble {
     constructor(x, y) {
         this.x = random(0, windowWidth);
-        this.y = random(0, windowHeight);
-        this.width = 50;
-        this.height = 50;
+        this.y = random(-windowHeight - 200, -50);
+        this.radius = random(25, 75);
         this.framesAlive = 0;
-        this.direction = createVector(0, 0);
+        this.movementOffset = random(0, 360);
     }
 
     draw() {
         fill(100, 100, 255);
-        ellipse(this.x, this.y, this.height, this.width);
+        ellipse(this.x, this.y, this.radius, this.radius);
 
         this.update();
     }
 
     update() {
-        if (this.framesAlive % 30 == 0) {this.changeDirection();}
-
-        this.x += this.direction.x;
-        this.y += this.direction.y;
-        this.smoothMovement();
-
+        this.y += 1;
+        this.x += cos(this.framesAlive/100 + this.movementOffset);
+        this.radius += cos((this.framesAlive + this.movementOffset)/30) * 0.25;
+        
+        if (this.y - this.radius*2 > windowHeight) {this.y = 0 - this.radius*2};
         this.framesAlive += 1;
-    }
-
-    changeDirection() {
-        console.log(this.direction.x);
-        this.direction.x = (random() * 1.5) * random([1, -1]);
-        this.direction.y = (random() * 1.5) * random([1, -1]);
-    }
-
-    smoothMovement() {
-        this.direction.x -= random()/10;
-        this.direction.y -= random()/10;
     }
 }
