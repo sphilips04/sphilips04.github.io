f13403c1410a2f7f2f9ab42aa44b85359dbe031e
sphilips04 <sphilips@email.sc.edu>
2024-02-08 02:02:15 -0500

    added some blue dots to the screen

--- InfoBubble.js
+++ InfoBubble.js
@@ -1,6 +1,17 @@
 class InfoBubble {
-    constructor() {
+    constructor(xScale, yScale) {
         //TODO
         //write a function to scale the bubbles to the size of the screen
+
+        this.xScale = xScale;
+        this.yScale = yScale;
+        this.radius = 150;
+
+    }
+
+    draw() {
+        noStroke();
+        fill(50, 50, 255);
+        ellipse(windowWidth * this.xScale, windowHeight * this.yScale, windowHeight * .15, windowHeight * .15);
     }
 }
--- homeMenu.js
+++ homeMenu.js
@@ -1,11 +1,20 @@
 let canvas;
 let bubbles;
+let infoBubbles;
 
 function setup() {
     bubbles = new Array();
     for (let i = 0; i < 50; i++) {
         let bubble = new Bubble();
         bubbles.push(bubble);
+    }
+
+    infoBubbles = new Array();
+    for (let i = 1; i <= 2; i++) {
+        infoBubbles.push(new Array())
+        for (let j = 1; j <= 6; j++) {
+            infoBubbles[i-1].push(new InfoBubble(j/10 * 1.45, i/10 * 3.2));
+        }
     }
 
     canvas = createCanvas(windowWidth, windowHeight);
@@ -16,6 +25,7 @@ function draw() {
     background(255);
 
     bubbles.forEach((bubble) => bubble.draw());
+    infoBubbles.forEach((array) => array.forEach((infoBubble) => infoBubble.draw()));
 }
 
 function windowResized() {
