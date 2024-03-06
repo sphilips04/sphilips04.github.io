3738d4ddde719b4f90690f19e6ba40927d1256d2
sphilips04 <sphilips@email.sc.edu>
2024-03-06 15:06:03 -0500

    added my name to the home page

--- NameBubble.js
+++ NameBubble.js
@@ -6,12 +6,13 @@ class NameBubble {
         this.timeSeconds = 0;
         this.frames = [[0,0],[1,0],[2,0],[0,1],[1,1],[2,1]];
         this.yFrame = 0;
-        this.radius = 32;
+        this.radius = 40;
+        this.color = random([[216, 17, 89]])
     }
 
     draw(bubbleAnimation) {
 
-        fill(0, 0, 255, 50);
+        fill(...this.color, 150);
         ellipse(this.x, this.y, this.radius-10, this.radius-10);
         imageMode(CENTER);
         let currentFrame = this.frames[this.timeSeconds];
@@ -23,7 +24,7 @@ class NameBubble {
     }
 
     update() {
-        this.time++;
+        //this.time++;
         if (this.time == 30) {
             this.time = 0;
             this.timeSeconds++;
--- homeMenu.js
+++ homeMenu.js
@@ -24,6 +24,21 @@ function setup() {
     }
 
     nameBubbles = new Array();
+    [
+        [433, 301], [406, 283], [371, 287], [348, 318], [342, 361],
+        [354, 399], [388, 426], [413, 448], [435, 474], [427, 507],
+        [400, 526], [362, 526], [329, 506], [476, 409], [475, 446],
+        [477, 486], [477, 525], [478, 562], [480, 595], [510, 406],
+        [547, 419], [553, 455], [526, 486], [596, 412], [595, 450],
+        [598, 489], [597, 526], [631, 529], [672, 532], [631, 410],
+        [674, 409], [633, 470], [719, 515], [718, 485], [718, 454],
+        [733, 425], [762, 416], [794, 426], [810, 457], [815, 489],
+        [816, 515], [923, 438], [896, 422], [862, 428], [851, 463],
+        [858, 500], [882, 525], [926, 517], [972, 405], [972, 441],
+        [974, 475], [974, 514], [1008, 518], [1042, 521], [1013, 399],
+        [1047, 398], [1006, 457], [1096, 405], [1094, 442], [1096, 473],
+        [1094, 510], [1094, 544], [1124, 429], [1151, 414], [1182, 413], [1210, 431]
+    ].forEach((posArr) => nameBubbles.push(new NameBubble(posArr[0], posArr[1] - 50)))
 
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0,0);
@@ -34,12 +49,17 @@ function draw() {
 
     bubbles.forEach((bubble) => bubble.draw());
     fill(255,0,0)
-    nameBubbles.forEach((bubble) => ellipse(bubble[0], bubble[1], 32, 32));
+    nameBubbles.forEach((bubble) => bubble.draw(bubblesAnimation));
 
 
     fill(255,0,0)
-    ellipse(mouseX, mouseY, 32, 32);
     //infoBubbles.forEach((array) => array.forEach((infoBubble) => infoBubble.draw()));
+
+
+    //stroke(100);
+    //for (let i = 0; i < 4; i++)
+    //    line(windowWidth * (i * .25), 0, windowWidth * (i * .25), windowHeight);
+    //line(0, windowHeight/2, windowWidth, windowHeight/2);
 }
 
 function windowResized() {
@@ -58,7 +78,7 @@ function mouseWheel(event) {
 }
 
 function mouseClicked() {
-    nameBubbles.push([mouseX, mouseY]);
+    
 }
 
 function logName() {
